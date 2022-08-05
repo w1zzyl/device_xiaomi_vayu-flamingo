e40166bb0ecbf9ddbe0cf41e735551fb9fa59c74
w1zzyl <w1zzyl@protonmail.com>
2022-08-05 17:56:11 +0200

    vayu: Adapt for Flamingo
    Signed-off-by: w1zzyl <w1zzyl@protonmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,6 @@
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/flamingo_vayu.mk \
+
+# Lunch targets
+COMMON_LUNCH_CHOICES += \
+    flamingo_vayu-userdebug
---
+++ flamingo_vayu.mk
@@ -0,0 +1,38 @@
+
+#
+# Copyright (C) 2021 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Check for target product
+ifeq (flamingo_vayu,$(TARGET_PRODUCT))
+
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+# Inherit from the AOSPA configuration
+$(call inherit-product, vendor/flamingo/target/product/flamingo.mk)
+
+PRODUCT_NAME := flamingo_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := Poco X3 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vayu_global-user 12 SKQ1.211006.001 V13.0.3.0.SJUMIXM release-keys" \
+    PRODUCT_NAME=vayu_global \
+    PRODUCT_MODEL=M2102J20SI
+
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.3.0.SJUMIXM:user/release-keys
+endif
